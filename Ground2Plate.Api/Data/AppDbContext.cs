990a8050e87a78bcbb17bdbe654638d4d1238759
LAPTOP-SATVHRTF\Kayla <Kayla.huber23@gmail.com>
2024-05-08 10:39:33 -0500

    Added migration

---
+++ Ground2Plate.Api/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Ground2Plate.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ground2Plate.Api.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; } = default!;
+
+    }
+}
--- Ground2Plate.Api/Program.cs
+++ Ground2Plate.Api/Program.cs
@@ -1,3 +1,6 @@
+using Ground2Plate.Api.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection") ??
+    throw new InvalidOperationException("Connection 'DefaultConnection' is not found."));
+});
+
 
 var app = builder.Build();
 
